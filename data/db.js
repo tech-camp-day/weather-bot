b08afda0a8734d9835cb9c0cd6e93cb8aed6d3e5
Plai <plai.rt.w@gmail.com>
2024-03-08 09:23:42 +0000

    Add weather code and db

--- data/db.js
+++ data/db.js
@@ -4,21 +4,60 @@ const db = require('better-sqlite3')('weather-bot.db', { verbose: console.log })
  * กำหนดค่าเริ่มต้นให้ฐานข้อมูลโดยการสร้างตารางที่จำเป็นหากยังไม่มีอยู่
  */
 const initDb = () => {
-  const createVocabTable = db.prepare(`
-      create table if not exists vocabs (
-        id integer primary key autoincrement,
-        word text not null,
-        meaning text not null
+  const createProvinceTable = db.prepare(`
+      create table if not exists provinces (
+        id integer primary key,
+        name text
+      )
+    `);
+
+  const createUserTable = db.prepare(`
+      create table if not exists users (
+        lineUserId text primary key,
+        provinceId integer,
+        foreign key(provinceId) references provinces(id)
       )
     `);
 
   const createAllTables = db.transaction(() => {
-    createVocabTable.run();
+    createProvinceTable.run();
     createUserTable.run();
-    createUserHistoryTable.run();
   });
 
   createAllTables();
 };
 
-module.exports = { initDb };
+initDb();
+
+function createUser(lineUserId, provinceId) {
+  const insertUser = db.prepare('insert into users (lineUserId, provinceId) values (?, ?)');
+  insertUser.run(lineUserId, provinceId);
+}
+
+function updateUser(lineUserId, provinceId) {
+  const updateUser = db.prepare('update users set provinceId = ? where lineUserId = ?');
+  updateUser.run(provinceId, lineUserId);
+}
+
+function searchProvinceByName(name) {
+  const selectProvince = db.prepare('select id, name from provinces where name = ?');
+  return selectProvince.get(name);
+}
+
+function getProvincesThatHasUser() {
+  const selectProvinces = db.prepare('select distinct p.id, p.name from provinces p join users u on p.id = u.provinceId');
+  return selectProvinces.all();
+}
+
+function getUserByProvinceId(provinceId) {
+  const selectUsers = db.prepare('select lineUserId from users where provinceId = ?');
+  return selectUsers.all(provinceId);
+}
+
+module.exports = {
+  createUser,
+  updateUser,
+  searchProvinceByName,
+  getProvincesThatHasUser,
+  getUserByProvinceId
+};
